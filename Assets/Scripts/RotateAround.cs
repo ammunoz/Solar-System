d0ed0be039332f951cddc50c25111d55e1821c97
ammunoz <munozamb@gmail.com>
2016-04-24 23:45:02 -0600

    Version 1

--- Assets/Scripts/RotateAround.cs
+++ Assets/Scripts/RotateAround.cs
@@ -6,19 +6,25 @@ using System.Collections;
 
 public class RotateAround : MonoBehaviour {
 
-	public Transform target; // the object to rotate around
+    public Transform target; // the object to rotate around
     public Vector3 axis; // axis on which object rotates
-    public int speed; // the speed of rotation
-	
-	void Start() {
-		if (!target) target = this.gameObject.transform;
-        if (axis.Equals(new Vector3(0, 0, 0))) axis = target.transform.up;
-	}
+    public float speed; // the speed of rotation - degree of rotation per second
+    public float speed_multiplier;
 
-	void Update () {
-		// RotateAround takes three arguments, first is the Vector to rotate around
-		// second is a vector that axis to rotate around
-		// third is the degrees to rotate, in this case the speed per second
-		transform.RotateAround(target.transform.position, axis, speed * Time.deltaTime);
-	}
+    void Start() {
+        if (!target) target = this.gameObject.transform;
+        if (axis.Equals(new Vector3(0, 0, 0))) axis = target.transform.up;
+        if (speed_multiplier==0) speed_multiplier = 1;
+    }
+
+    // RotateAround takes three arguments, first is the Vector to rotate around
+    // second is a vector that axis to rotate around
+    // third is the degrees to rotate, in this case the speed per second
+    void Update() {
+        transform.RotateAround(target.transform.position, axis, -(speed*speed_multiplier) * Time.deltaTime);
+    }
+
+    public float SpeedMultiplier{
+        set{ speed_multiplier = value; }
+    }
 }
---
+++ Assets/Scripts/SwitchView.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SwitchView : MonoBehaviour {
+
+    int viewMode;
+	// Use this for initialization
+	void Start () {
+        viewMode = 0;
+	}
+
+    public void switchView(){
+        viewMode ^= 1;
+        if (viewMode == 1)
+        {
+            this.gameObject.transform.Rotate(new Vector3(90, 0, 0));
+            this.gameObject.transform.position = new Vector3(0, 3, 0);
+        }
+        else {
+            this.gameObject.transform.rotation = new Quaternion(0, 0, 0, 1);
+            this.gameObject.transform.position = new Vector3(0, 0, -3);
+        }
+    }
+}
